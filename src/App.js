587bee9966970f672f93b91c62a19ffd52458baf
Harsh Sharma <harsh464565@gmail.com>
2023-05-05 20:33:34 +0530

    developed my first react app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import MovieCard from "./MovieCard";
+
+import { useEffect, useState } from "react";
+const React = require("react");
+
+const API_URL = "http://www.omdbapi.com?apikey=3c202bf8";
+
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <span className="searchTerm" onClick={() => searchMovies(searchTerm)}>
+          Search
+        </span>
+      </div>
+
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+// 3c202bf8
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(<App />, document.getElementById("root"));
